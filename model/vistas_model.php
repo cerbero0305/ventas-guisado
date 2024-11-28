e393d4919bd5fb5d7502000ac3e99f548a763ff6
cerbero0305 <zon03ab@gmail.com>
2024-11-28 18:22:26 -0500

    Update vistas_model.php

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -3,9 +3,9 @@ session_start();
     class vistaModelo{
         protected static function obtener_vistas($vista){
             $nombres_permitidos = ['inicio', 'tendencias', 'youtube', 'musica', 'yo', 'informacion', 'buscar','ingresarNuevoProducto','productos','ingresarCompras','editarProducto','ingresarCategoria','verCategoria','ingreserPersona','verPersona'];
-            /*if (!isset($_SESSION['sesion_ventas_id'])) {
+            if (!isset($_SESSION['sesion_ventas_id'])) {
                 return "login";
-            }*/
+            }
             if (in_array($vista, $nombres_permitidos)) {
                 if (is_file("./views/".$vista.".php")) {
                     $contenido = "./views/".$vista.".php";
